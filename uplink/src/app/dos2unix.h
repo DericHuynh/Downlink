2368c72463b7b591a9fa2766c9a59126812c8890
DericHuynh <HuynhDeric@gmail.com>
2025-07-30 16:29:09 -0600

    Initial Uplink codebase that compiles
    Uplink codebase updated to Visual Studio 2022 that compiles.

---
+++ uplink/src/app/dos2unix.h
@@ -0,0 +1,42 @@
+#if !defined __DOS2UNIX_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class dos2unixbuf : public streambuf {
+public:
+  typedef ios::openmode openmode;
+
+  dos2unixbuf(const char *filename, openmode mode);
+  virtual ~dos2unixbuf();
+
+  void close();
+
+protected:
+  virtual int overflow(int c = EOF);
+  virtual int underflow();
+  virtual int uflow();
+  virtual int pbackfail(int c = EOF);
+  virtual int sync();
+  
+private:
+  filebuf inner;
+  char *buffer;
+};
+
+class idos2unixstream
+  : public istream {
+public:
+  idos2unixstream(const char *filename)
+    : istream(new dos2unixbuf(filename, ios::in))
+    {
+    }
+
+  virtual ~idos2unixstream();
+
+  void close();
+};
+
+#endif // __DOS2UNIX_H
